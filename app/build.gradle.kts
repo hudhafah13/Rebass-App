18071474dd00bbe38750c26a60b8e0d33092a3d2
hudhafah13 <hellohudhafah@gmail.com>
2026-09-06 13:16:25 +0200

    Fix Java/Kotlin JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,17 +2,29 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
-    namespace="com.rebass.app"
-    compileSdk=35
+    namespace = "com.rebass.app"
+    compileSdk = 35
+
     defaultConfig {
-        applicationId="com.rebass.app"
-        minSdk=26
-        targetSdk=35
-        versionCode=2
-        versionName="1.0"
+        applicationId = "com.rebass.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0"
+
         ndk {
             abiFilters.add("armeabi-v7a")
         }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
